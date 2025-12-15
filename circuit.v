520e64a61d533e243857223135d32173217801f7
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-15 17:24:25 -0500

    Modify 'invert' module to expand inputs and outputs
    Updated module 'invert' to include additional inputs and outputs.

--- circuit.v
+++ circuit.v
@@ -1,6 +1,8 @@
 `timescale 1ns / 1ps
-module invert(x,r,q1,q2);
+module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11;);
 
-  input x1,x2,x3,r;
-  output q1, q2, o1;
-  wire
+  input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,d,j,k,t_clk;
+  output q1,q2;
+  wire dp1,s,r,dp2, jk1, jk2, jk3;
+
+  
