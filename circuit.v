1f069729f2eaab95dfbaa829885ba9d62ca7d367
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-15 17:25:19 -0500

    Fix module declaration syntax in circuit.v

--- circuit.v
+++ circuit.v
@@ -1,5 +1,5 @@
 `timescale 1ns / 1ps
-module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11;);
+module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11);
 
   input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,d,j,k,t_clk;
   output q1,q2;
