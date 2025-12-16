27a08c7cdbb990749e2016dcc92d494d3e8e75e7
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-16 14:34:25 -0500

    Refactor input and output ports in circuit.v

--- circuit.v
+++ circuit.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11);
 
-  input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,d,j,k,t_clk;
-  output q1,q2;
-  wire dp1,s,r,dp2, jk1, jk2, jk3;
+  input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,j,k,t_clk;
+  output q,nq;
+  wire pd1,s,r,pd2, jk1, jk2, jk3;
 
+  
   
