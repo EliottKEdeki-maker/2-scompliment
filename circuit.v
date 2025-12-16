7c99c6c7a7a3a1667f4fa0edacfda7a3512a4ac5
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-16 15:26:48 -0500

    Add NAND gates for signal processing

--- circuit.v
+++ circuit.v
@@ -9,4 +9,10 @@ module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r
   not (#5) (nk, k);
   and (#10) (jk2, nk, q);
   or (#10) (d, jk1, jk2);
-  
+  nand (#12) (pd1, s, pd2);
+  nand (#12) (s, t_clk, pd1);
+  nand (#12) (r, t_clk, s, pd2);
+  nand (#12) (pd2, d, r);
+  nand (#12) (q, s, nq);
+  nand (#12) (nq, r, q);
+  (#) 
