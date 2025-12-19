f930e79d680f81acb0277c1a78612f89d3642480
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-19 15:29:58 -0500

    Update circuit.v

--- circuit.v
+++ circuit.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,j,k,t_clk,o0,o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11);
 
-  input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,j,k,t_clk,o0,o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11;
+  input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,j,k,t_clk;
   output o0,o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11;
   wire jk1, jk2, q, nq;
   
