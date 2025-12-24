4e38f5d9589fcd43945f32653148da08e9e882f7
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-24 16:39:47 -0500

    Reorder nand gate connections in invert module

--- circuit.v
+++ circuit.v
@@ -3,14 +3,14 @@ module invert(x,r,t_clk,y);
 
   input x,r,t_clk;
   output y;
-  wire jk1, jk2, q, nq, i;
+  wire jk1, jk2, q, nq;
 
- nand #(12) (jk1, x, t_clk, nq);
- nand #(12) (jk2, r, t_clk, q);
- nand #(12) (q, jk1, nq);
- nand #(12) (nq, jk2, q );
+  nand #(12) (jk1, r, t_clk, nq);
+  nand #(12) (jk2, x, t_clk, q);
+  nand #(12) (q, jk1, nq);
+  nand #(12) (nq, jk2, q);
 
- and #(10) (i );
+  
  
 endmodule
-
+ 
