23bfde3b3956adcbd3d834e8dba423893791b7db
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-24 17:18:32 -0500

    Add input 'i' and update output logic with XNOR

--- circuit.v
+++ circuit.v
@@ -3,14 +3,13 @@ module invert(x,r,t_clk,y);
 
   input x,r,t_clk;
   output y;
-  wire jk1, jk2, q, nq;
+  wire jk1, jk2, q, nq, i;
 
   nand #(12) (jk1, x, t_clk, nq);
   nand #(12) (jk2, r, t_clk, q);
   nand #(12) (q, jk1, nq);
   nand #(12) (nq, jk2, q);
 
-  
- 
+  xnor #(10) (y, nq, q);
 endmodule
  
