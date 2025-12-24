e9f863208681398c12ad462dadb6fdd23ac2db3f
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-23 23:20:49 -0500

    Refactor invert instance in testbed.v

--- testbed.v
+++ testbed.v
@@ -4,7 +4,7 @@ module tc;
 wire ;
 reg ;
 initial begin t_clock = 0; forever #5 t_clock = ~t_clock; end
-  invert I1 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,t_clock,q,nq);
+  invert I1 (x,r,t_clock,y);
 
 initial
   begin
