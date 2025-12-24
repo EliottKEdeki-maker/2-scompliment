402c350c4608d686fc033e646b242d4508ca58d9
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-23 22:00:05 -0500

    Fix AND gate instantiation in circuit.v
    Added missing connections to the AND gate.

--- circuit.v
+++ circuit.v
@@ -4,13 +4,15 @@ module invert(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r
   input x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,t_clk;
   output o0,o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11;
   wire jk1, jk2, q, nq;
+
+  buf #(4) (j, r0);
   
   nand #(12) (jk1, j, t_clk, nq);
   nand #(12) (jk2, k, t_clk, q);
   nand #(12) (q, jk1, nq);
   nand #(12) (nq, jk2, q);
 
-  and #(10) ;
+  and #(10) ( , q, );
   
 
-  
+endmodule
