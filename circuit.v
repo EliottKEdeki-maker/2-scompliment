3eb314c00fbd4cabcc873eb70c328dd34e7947aa
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-24 12:09:32 -0500

    Simplify inverter module in circuit.v
    Refactor circuit.v by removing unused wires and simplifying logic.

--- circuit.v
+++ circuit.v
@@ -1,17 +1,16 @@
- `timescale 1ns / 1ps
+`timescale 1ns / 1ps
 module invert(x,r,t_clk,y);
 
   input x,r,t_clk;
   output y;
-  wire j, k, jk1, jk2, q, nq;
- 
-  not #(5) (j,r);
-  buf #(4) (k,r);
-  nand #(12) (jk1, j, t_clk, nq);
-  nand #(12) (jk2, k, t_clk, q);
-  nand #(12) (q, jk1, nq);
-  nand #(12) (nq, jk2, q);
-  
+  wire jk1, jk2, q, nq, i;
 
+ nand #(12) (jk1, x, t_clk, nq);
+ nand #(12) (jk2, r, t_clk, q);
+ nand #(12) (q, jk1, nq);
+ nand #(12) (nq, jk2, q );
+
+ and #(10) (i );
+ 
 endmodule
 
