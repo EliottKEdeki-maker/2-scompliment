7a4b5f85836fcfcab904fd0288f419901918bb7d
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-25 13:37:43 -0500

    Add clock signal to testbench stimulus
    Updated the testbench to include clock signal in stimulus.

--- testbed.v
+++ testbed.v
@@ -1,27 +1,27 @@
 `timescale 1ns / 1ps
 module tc;
 
-wire ;
-reg ;
-initial begin t_clock = 0; forever #5 t_clock = ~t_clock; end
+wire y;
+reg x,r;
+initial begin t_clock = 0; forever #70 t_clock = ~t_clock; end
   invert I1 (x,r,t_clock,y);
 
 initial
   begin
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
-    #78 x = 1'b1, r = 1'b1;
-    #78 x = 1'b0, r = 1'b0;
-    #78 x = 1'b1, r = 1'b0;
-    #78 x = 1'b0, r = 1'b0;
-    #78 x = 1'b1, r = 1'b1;
-    #78 x = 1'b1, r = 1'b0;
-    #78 x = 1'b0, r = 1'b0;
-    #78 x = 1'b1, r = 1'b0;
-    #78 x = 1'b0, r = 1'b0;
-    #78 x = 1'b0, r = 1'b0;
-    #78 x = 1'b1, r = 1'b0;
-    #78 x = 1'b1, r = 1'b0;
+    #78 x = 1'b1, r = 1'b1, t_clock;
+    #78 x = 1'b0, r = 1'b0, t_clock;
+    #78 x = 1'b1, r = 1'b0, t_clock;
+    #78 x = 1'b0, r = 1'b0, t_clock;
+    #78 x = 1'b1, r = 1'b1, t_clock;
+    #78 x = 1'b1, r = 1'b0, t_clock;
+    #78 x = 1'b0, r = 1'b0, t_clock;
+    #78 x = 1'b1, r = 1'b0, t_clock;
+    #78 x = 1'b0, r = 1'b0, t_clock;
+    #78 x = 1'b0, r = 1'b0, t_clock;
+    #78 x = 1'b1, r = 1'b0, t_clock;
+    #78 x = 1'b1, r = 1'b0, t_clock;
 
   end
   initial #937 $finish;
