db295975fd02f118059a6354081a9519c51b2ddc
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-25 15:32:14 -0500

    Refactor testbed.v by removing duplicate code

--- testbed.v
+++ testbed.v
@@ -1,10 +1,9 @@
 `timescale 1ns / 1ps
 module tc;
-
-wire y;
-reg x,r;
-initial begin t_clock = 0; forever #100 t_clock = ~t_clock; end
-invert I1 (x,r,t_clock,y);
+  wire y;
+  reg x,r;
+  initial begin t_clock = 0; forever #100 t_clock = ~t_clock; end
+  invert I1 (x,r,t_clock,y);
 
 initial
   begin
