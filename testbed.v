7121bf45321e18c0aefd04c97155f0ea72ede081
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-25 15:54:22 -0500

    Update testbed.v

--- testbed.v
+++ testbed.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 module tc;
   wire y;
-  reg x,r;
+  reg i,r;
   initial begin t_clock = 0; forever #100 t_clock = ~t_clock; end
   invert I1 (x,r,t_clock,y);
 
@@ -9,18 +9,18 @@ initial
   begin
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
-    #78 X = 1'b1, r = 1'b1, t_clock;
-    #78 x = 1'b0, r = 1'b0, t_clock;
-    #78 x = 1'b1, r = 1'b0, t_clock;
-    #78 x = 1'b0, r = 1'b0, t_clock;
-    #78 x = 1'b1, r = 1'b1, t_clock;
-    #78 x = 1'b1, r = 1'b0, t_clock;
-    #78 x = 1'b0, r = 1'b0, t_clock;
-    #78 x = 1'b1, r = 1'b0, t_clock;
-    #78 x = 1'b0, r = 1'b0, t_clock;
-    #78 x = 1'b0, r = 1'b0, t_clock;
-    #78 x = 1'b1, r = 1'b0, t_clock;
-    #78 x = 1'b1, r = 1'b0, t_clock;
+    #78 i = 1'b1, r = 1'b1, t_clock;
+    #78 i = 1'b0, r = 1'b0, t_clock;
+    #78 i = 1'b1, r = 1'b0, t_clock;
+    #78 i = 1'b0, r = 1'b0, t_clock;
+    #78 i = 1'b1, r = 1'b1, t_clock;
+    #78 i = 1'b1, r = 1'b0, t_clock;
+    #78 i = 1'b0, r = 1'b0, t_clock;
+    #78 i = 1'b1, r = 1'b0, t_clock;
+    #78 i = 1'b0, r = 1'b0, t_clock;
+    #78 i = 1'b0, r = 1'b0, t_clock;
+    #78 i = 1'b1, r = 1'b0, t_clock;
+    #78 i = 1'b1, r = 1'b0, t_clock;
 
   end
   initial #937 $finish;
