c9fa8be2d49f61079a36fe94c63846b88d891b2b
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-26 21:38:23 -0500

    Update testbed.v

--- testbed.v
+++ testbed.v
@@ -2,7 +2,7 @@
 module tc;
   wire y;
   reg i,r,t_clk;
-  initial begin t_clk = 0; forever #82 t_clk = ~t_clk; end
+  initial begin t_clk = 0; forever #95 t_clk = ~t_clk; end
   invert I1 (i,r,t_clk,y);
 
 initial
