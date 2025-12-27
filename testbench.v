d6760783215f55b54c2f5777d0923f1d829cb725
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-26 23:53:00 -0500

    Update and rename testbed.v to testbench.v

--- testbench.v
+++ testbench.v
@@ -9,7 +9,7 @@ initial
   begin
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
-    i = 0; r = 0;
+    i = 1'b0; r = 1'b1;
     #160 i = 1'b1; r = 1'b1;
     #160 i = 1'b0; r = 1'b0;
     #160 i = 1'b1;
