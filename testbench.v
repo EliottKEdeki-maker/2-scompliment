e42bd5709bef3cbb5dfd1db0d9dca5ce10ded0a9
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-27 09:49:38 -0500

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -2,27 +2,28 @@
 module tc;
   wire y;
   reg i,r,t_clk;
-  initial begin t_clk = 0; forever #50 t_clk = ~t_clk; end
+  initial begin t_clk = 0; forever #78 t_clk = ~t_clk; end
   invert I1 (i,r,t_clk,y);
 
 initial
   begin
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
-    i = 1'b0; r = 1'b1;
-    #100
-    #20 i = 1'b0; r = 1'b0;
-    #20 i = 1'b1;
-    #20 i = 1'b0; 
-    #20 i = 1'b1; r = 1'b1;
-    #20 i = 1'b1; r = 1'b0;
-    #20 i = 1'b0; 
-    #20 i = 1'b1; 
-    #20 i = 1'b0; 
-    #20 i = 1'b0; 
-    #20 i = 1'b1; 
-    #20 i = 1'b1; 
+    i = 1'b1; r = 1'b1;
+    #156;
+    #78 i = 1'b0; r = 1'b1;
+    #156 i = 1'b0; r = 1'b0;
+    #156 i = 1'b1;
+    #156 i = 1'b0; 
+    #156 i = 1'b1; r = 1'b1;
+    #156 i = 1'b1; r = 1'b0;
+    #156 i = 1'b0; 
+    #156 i = 1'b1; 
+    #156 i = 1'b0; 
+    #156 i = 1'b0; 
+    #156 i = 1'b1; 
+    #156 i = 1'b1; 
 
   end
-  initial #1880 $finish;
+  initial #2000 $finish;
 endmodule
