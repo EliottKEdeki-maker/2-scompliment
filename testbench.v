4758eca6ab0e43a733af9220cb894d2153453c98
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-27 07:54:33 -0500

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -2,7 +2,7 @@
 module tc;
   wire y;
   reg i,r,t_clk;
-  initial begin t_clk = 0; forever #156 t_clk = ~t_clk; end
+  initial begin t_clk = 0; forever #90 t_clk = ~t_clk; end
   invert I1 (i,r,t_clk,y);
 
 initial
