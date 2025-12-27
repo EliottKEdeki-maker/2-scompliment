5ffc1b79477d9028f0eb14e51a96bfbee418aec9
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-27 08:18:37 -0500

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -2,7 +2,7 @@
 module tc;
   wire y;
   reg i,r,t_clk;
-  initial begin t_clk = 0; forever #90 t_clk = ~t_clk; end
+  initial begin t_clk = 0; forever #150 t_clk = ~t_clk; end
   invert I1 (i,r,t_clk,y);
 
 initial
@@ -10,17 +10,18 @@ initial
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
     i = 1'b0; r = 1'b1;
-    #156 i = 1'b0; r = 1'b0;
-    #156 i = 1'b1;
-    #156 i = 1'b0; 
-    #156 i = 1'b1; r = 1'b1;
-    #156 i = 1'b1; r = 1'b0;
-    #156 i = 1'b0; 
-    #156 i = 1'b1; 
-    #156 i = 1'b0; 
-    #156 i = 1'b0; 
-    #156 i = 1'b1; 
-    #156 i = 1'b1; 
+    #100
+    #20 i = 1'b0; r = 1'b0;
+    #20 i = 1'b1;
+    #20 i = 1'b0; 
+    #20 i = 1'b1; r = 1'b1;
+    #20 i = 1'b1; r = 1'b0;
+    #20 i = 1'b0; 
+    #20 i = 1'b1; 
+    #20 i = 1'b0; 
+    #20 i = 1'b0; 
+    #20 i = 1'b1; 
+    #20 i = 1'b1; 
 
   end
   initial #1880 $finish;
