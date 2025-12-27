30fccfde3b4f4431282e06d444f9bc69ff7313ed
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-26 21:27:37 -0500

    Update circuit.v

--- circuit.v
+++ circuit.v
@@ -3,12 +3,13 @@ module invert(i,r,t_clk,y);
 
   input  i,r,t_clk;
   output y;
-  wire in, K, jk1, jk2, Q, NQ, kn, D, d1, S, R, d4, rn, Qrn;
+  wire J, in, K, jk1, jk2, Q, NQ, kn, D, d1, S, R, d4, rn, Qrn;
 
+  buf #(4) (J, i);
   not #(5) (in, i);
   and #(10) (K, in, r);
   not #(5) (kn, K);
-  and #(10) (jk1, i, NQ);
+  and #(10) (jk1, J, NQ);
   and #(10) (jk2, kn, Q);
   or #(10) (D, jk1, jk2);
 
