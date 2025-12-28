f9065180ee251f6f4d95f9cb1ed56a46183db28d
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-28 04:08:00 -0500

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -9,7 +9,7 @@ initial
   begin
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
-    //i = 1'b0; r = 1'b1; intialization value so flip flop isn't x value at start
+    i = 1'b0; r = 1'b1; //intialization value so flip flop isn't x value at start
     #156;
     #40; i = 1'b1; r = 1'b1;
     #156; i = 1'b0; r = 1'b0;
