aab8f40e9c01e3eb9e538127c56146029c57f896
EliottKEdeki-maker <eliott.k.edeki@gmail.com>
2025-12-28 06:57:09 -0500

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -2,7 +2,7 @@
 module tc;
   wire y;
   reg i,r,t_clk;
-  initial begin t_clk = 0; forever #54 t_clk = ~t_clk; end // total delay of ciruit is 156 so set half of clock period to 156/2 = 78 
+  initial begin t_clk = 0; forever #53 t_clk = ~t_clk; end // total delay of ciruit is 156 so set half of clock period to 156/2 = 78 
   invert I1 (i,r,t_clk,y);
 
 initial
@@ -10,20 +10,20 @@ initial
     $dumpfile("wave.vcd");
     $dumpvars(0, tc, I1);
     i = 1'b0; r = 1'b1; //intialization value so flip flop with one full clock period to get rid of x values                                           
-    #108; i = 1'b1; r = 1'b1; // input bits least to most signifiant
-    #108; i = 1'b0; r = 1'b0;
-    #108; i = 1'b1; r = 1'b0;
-    #108; i = 1'b0; r = 1'b0;
-    #108; i = 1'b1; r = 1'b1;
-    #108; i = 1'b1; r = 1'b0;
-    #108; i = 1'b0; r = 1'b0;
-    #108; i = 1'b1; r = 1'b0;
-    #108; i = 1'b0; r = 1'b0;
-    #108; i = 1'b0; r = 1'b0;
-    #108; i = 1'b1; r = 1'b0;
-    #108; i = 1'b1; r = 1'b0;
+    #106; i = 1'b1; r = 1'b1; // input bits least to most signifiant
+    #106; i = 1'b0; r = 1'b0;
+    #106; i = 1'b1; r = 1'b0;
+    #106; i = 1'b0; r = 1'b0;
+    #106; i = 1'b1; r = 1'b1;
+    #106; i = 1'b1; r = 1'b0;
+    #106; i = 1'b0; r = 1'b0;
+    #106; i = 1'b1; r = 1'b0;
+    #106; i = 1'b0; r = 1'b0;
+    #106; i = 1'b0; r = 1'b0;
+    #106; i = 1'b1; r = 1'b0;
+    #106; i = 1'b1; r = 1'b0;
     
 
   end
-  initial #1296 $finish; // Total time accounts for all delays
+  initial #1272 $finish; // Total time accounts for all delays
 endmodule
